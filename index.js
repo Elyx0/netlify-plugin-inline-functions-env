d09071d0ee2783440edd85a122e08162cfbd903d
Elyx0 <elyx00@gmail.com>
2021-10-27 06:34:02 -0400

    Update manifest

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-const fs = require('fs')
-const util = require('util')
-const babel = require('@babel/core')
-const inlinePlugin = require('babel-plugin-transform-inline-environment-variables')
+const fs = require('fs');
+const util = require('util');
+const babel = require('@babel/core');
+const inlinePlugin = require('babel-plugin-transform-inline-environment-variables');
 const tsPlugin = require('@babel/plugin-transform-typescript');
-const { normalizeInputValue, isJsFunction, getSrcFile, uniq } = require('./lib')
-const writeFile = util.promisify(fs.writeFile)
+const { normalizeInputValue, isJsFunction, getSrcFile, uniq } = require('./lib');
+const writeFile = util.promisify(fs.writeFile);
+
+const path = require('path');
+
+const readDirRecursive = async (filePath) => {
+    const dir = await fs.promises.readdir(filePath);
+    const files = await Promise.all(dir.map(async realtivePath => {
+        const absolutePath = path.join(filePath, realtivePath);
+        const stat = await fs.promises.lstat(absolutePath);
+
+        return stat.isDirectory() ? readDirRecursive(absolutePath) : absolutePath;
+    }));
+
+    return files.flat();
+}
 
 async function inlineEnv(path, options = {}, verbose = false) {
-  console.log('inlining', path)
+  console.log('inlining', path);
 
   const transformed = await babel.transformFileAsync(path, {
     configFile: false,
     plugins: [babel.createConfigItem([tsPlugin, options]),babel.createConfigItem([inlinePlugin, options])],
     retainLines: true,
-  })
+  });
 
   if (verbose) {
     console.log('transformed code', transformed.code)
@@ -22,7 +36,7 @@ async function inlineEnv(path, options = {}, verbose = false) {
   await writeFile(path, transformed.code, 'utf8')
 }
 
-async function processFiles({ inputs, utils }) {
+async function processFiles({ inputs, constants, utils }) {
   const verbose = !!inputs.verbose
 
   if (verbose) {
@@ -35,7 +49,29 @@ async function processFiles({ inputs, utils }) {
   let netlifyFunctions = []
 
   try {
-    netlifyFunctions = await utils.functions.listAll()
+    netlifyFunctions = await utils.functions.listAll();
+
+    if (inputs.inlineAll) {
+      const additionalFiles = await readDirRecursive(constants.FUNCTIONS_SRC);
+    
+      if (verbose) {
+        console.log('Constants:',constants);
+        console.log('Loading from',constants.FUNCTIONS_SRC)
+        console.log('Found',additionalFiles)
+  
+      }
+      netlifyFunctions.push(...additionalFiles.map(file => {
+        const { ext } = path.parse(file);
+        const absolutePath = path.resolve(file); //path.resolve( constants.FUNCTIONS_SRC, file );
+        return {
+        runtime: ext.slice(1),
+        extension: ext,
+        srcFile: absolutePath,
+        }
+      }));
+    }
+
+    
   } catch (functionMissingErr) {
     console.log(functionMissingErr) // functions can be there but there is an error when executing
     return utils.build.failBuild(
